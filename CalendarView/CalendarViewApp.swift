e7ea2138e094099dfd360b38c986dc511ff18f4b
Vamsi Kallepalli <vkallepalli@uworld.com>
2024-08-08 10:38:47 -0500

    Initial Commit

---
+++ CalendarView/CalendarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarViewApp.swift
+//  CalendarView
+//
+//  Created by Vamsi Kallepalli on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
